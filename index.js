b5c1d674dc52b272871acd326ae5a32ff14e0908
imanariii <shark69kg@gmail.com>
2023-05-06 10:12:13 +0300

    Изменил параметры для бд vps

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 const app = express()
 app.use(cors())
